03f8c41b4a47485dde4621bbaf683489f6e808ad
piro0526 <piro0526@gmail.com>
2023-11-24 03:32:54 +0900

    i

--- src/Movehandler.cpp
+++ src/Movehandler.cpp
@@ -1,46 +1,66 @@
 #include "Movehandler.hpp"
 
-void MoveHandler::setNext()
+void MoveHandler::setNext(std::unique_ptr<MoveHandler> nextMoveHandler)
+{
+    _nextMoveHandler = std::move(nextMoveHandler);
+};
+
+bool CastlingMoveHandler::handleMove(Board& board, PieceMetadata metadata, Move move)
+{
+    if(isCastlingMove(metadata, move))
+    {
+        int rank = move.getStart().getRank();
+        int file = move.getStart().getFile();
+        int yDistance = move.getStart().getFile() - move.getEnd().getFile();
+        int direction = yDistance < 0 ? 1 : -1;
+        Move rookMove(move.getEnd(), Spot(rank, file + direction));
+        Move kingMove(move.getEnd(), Spot(rank, file + 3 * direction));
+        board.movePiece(rookMove);
+        board.movePiece(kingMove);
+
+        return true;
+    }
+    if(_nextMoveHandler != nullptr)
+    {
+        return _nextMoveHandler->handleMove(board, metadata, move);
+    }
+    else
+    {
+        return false;
+    }
+};
+
+bool CastlingMoveHandler::isCastlingMove(PieceMetadata metadata, Move move)
 {
 
 };
 
-void CastlingMoveHandler::handleMove(Board board, PieceMetadata metadata, Move move)
+bool EnPassantMoveHandler::handleMove(Board& board, PieceMetadata metadata, Move move)
 {
 
 };
 
-void CastlingMoveHandler::isCastlingMove(Board board, Move move)
+bool EnPassantMoveHandler::isEnPassantMove(PieceMetadata metadata, Move move)
 {
 
 };
 
-void EnPassantMoveHandler::handleMove(Board board, PieceMetadata metadata, Move move)
+bool PromotionMoveHandler::handleMove(Board& board, PieceMetadata metadata, Move move)
 {
 
 };
 
-void EnPassantMoveHandler::isEnPassantMove(Board board, Move move)
+bool PromotionMoveHandler::isPromotionMove(PieceMetadata metadata, Move move)
 {
 
 };
 
-void PromotionMoveHandler::handleMove(Board board, PieceMetadata metadata, Move move)
+bool RegularMoveHandler::handleMove(Board& board, PieceMetadata metadata, Move move)
 {
 
 };
 
-void PromotionMoveHandler::isPromotionMove(Board board, Move move)
-{
-
-};
-
-void RegularMoveHandler::handleMove(Board board, PieceMetadata metadata, Move move)
-{
-
-};
-
-void RegularMoveHandler::isRegularMove(Board board, Move move)
+bool RegularMoveHandler::isRegularMove(PieceMetadata metadata, Move move)
 {
 
 };
--- src/Movehandler.hpp
+++ src/Movehandler.hpp
@@ -5,7 +5,7 @@ class IMoveHandler
 {
 public:
     virtual void setNext(std::unique_ptr<IMoveHandler> nextMoveHandler)=0;
-    virtual bool handleMove(Board board, PieceMetadata metadata, Move move)=0;
+    virtual bool handleMove(Board& board, PieceMetadata metadata, Move move)=0;
 };
 
 class MoveHandler : public IMoveHandler
@@ -19,27 +19,27 @@ public:
 class CastlingMoveHandler : public MoveHandler
 {
 public:
-    bool handleMove(Board board, PieceMetadata metadata, Move move);
-    bool isCastlingMove(Board board, Move move);
+    bool handleMove(Board& board, PieceMetadata metadata, Move move);
+    bool isCastlingMove(PieceMetadata metadata, Move move);
 
 };
 
 class EnPassantMoveHandler : public MoveHandler
 {
 public:
-    bool handleMove(Board board, PieceMetadata metadata, Move move);
-    bool isEnPassantMove(Board board, Move move);
+    bool handleMove(Board& board, PieceMetadata metadata, Move move);
+    bool isEnPassantMove(PieceMetadata metadata, Move move);
 };
 
 class PromotionMoveHandler : public MoveHandler
 {
 public:
-    bool handleMove(Board board, PieceMetadata metadata, Move move);
-    bool isPromotionMove(Board board, Move move);
+    bool handleMove(Board& board, PieceMetadata metadata, Move move);
+    bool isPromotionMove(PieceMetadata metadata, Move move);
 };
 
 class RegularMoveHandler : public MoveHandler
 {
 public:
-    bool handleMove(Board board, PieceMetadata metadata, Move move);
+    bool handleMove(Board& board, PieceMetadata metadata, Move move);
 };
